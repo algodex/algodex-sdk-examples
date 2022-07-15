40b0f998d286690f9804ecfc17a942453ebda700
Alexander Trefonas <atrefonas@gmail.com>
2022-07-15 17:40:27 -0400

    Add initial tests

--- simple-market-making-bot/lib/cancelOrders.js
+++ simple-market-making-bot/lib/cancelOrders.js
@@ -1,20 +1,30 @@
+const convertCancelResultsToDBPromises = (escrowDB, results, orders) => {
+  const addrs = results.map(result => result[0].escrowAddr);
+  const resultAddrs = new Set(addrs);
+  const removeFromDBPromises = orders.rows
+      .filter(order => resultAddrs.has(order.doc.order.escrowAddr))
+      .map(order => escrowDB.remove(order.doc));
+  if (results.length > 0) {
+    console.log({results});
+  }
+  return removeFromDBPromises;
+};
 
 const cancelOrders = async (escrowDB, orders, cancelPromises) => {
-  await Promise.all(cancelPromises).then(async function(results) {
-    const addrs = results.map(result => result[0].escrowAddr);
-    const resultAddrs = new Set(addrs);
-    const removeFromDBPromises = orders.rows
-        .filter(order => resultAddrs.has(order.doc.order.escrowAddr))
-        .map(order => escrowDB.remove(order.doc));
-    if (results.length > 0) {
-      console.log({results});
-    }
-    await Promise.all(removeFromDBPromises).catch(function(e) {
+  return await Promise.all(cancelPromises).then(async function(results) {
+    const removeFromDBPromises =
+      convertCancelResultsToDBPromises(escrowDB, results, orders);
+    const result = await Promise.all(removeFromDBPromises).catch(function(e) {
       console.error(e);
     });
+    return result;
   }).catch(function(e) {
     console.error(e);
   });
 };
 
-module.exports = cancelOrders;
+if (process.env.NODE_ENV === 'test') {
+  module.exports = {cancelOrders, convertCancelResultsToDBPromises};
+} else {
+  module.exports = cancelOrders;
+}
---
+++ simple-market-making-bot/lib/cancelOrders.spec.js
@@ -0,0 +1,34 @@
+const {cancelOrders, convertCancelResultsToDBPromises}
+  = require ('./cancelOrders');
+
+const escrowDB = {
+  remove: jest.fn(doc => new Promise(resolve => resolve('done')))
+};
+
+const orders = {
+  rows: [
+    {
+      doc: {
+        order: {
+          escrowAddr: 'algorandaddr1'
+        }
+      }
+    }
+]};
+
+test('converts cancel results', () => {
+  const results = [[{'escrowAddr': 'algorandaddr1'}]];
+
+  const promises = convertCancelResultsToDBPromises(escrowDB, results, orders);
+  expect(escrowDB.remove.mock.calls.length).toBe(1);
+  const mockResults = escrowDB.remove.mock.calls[0][0];
+  expect(JSON.stringify(mockResults)).toBe(JSON.stringify({'order': results[0][0]}));
+});
+
+test('executes promises', async () => {
+  const promiseResults = [[{'escrowAddr': 'algorandaddr1'}]];
+
+  const results = 
+    await cancelOrders(escrowDB, orders, [new Promise(resolve => resolve(promiseResults[0]))]);
+  expect(results[0]).toBe('done');
+});
