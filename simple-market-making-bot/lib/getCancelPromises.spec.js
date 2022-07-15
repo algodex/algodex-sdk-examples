815e7174f64f15d7ea9c8ca6ed9ff653958e1f49
Alexander Trefonas <atrefonas@gmail.com>
2022-07-15 18:53:27 -0400

    Add getCancelPromises tests

---
+++ simple-market-making-bot/lib/getCancelPromises.spec.js
@@ -0,0 +1,55 @@
+const getCancelPromises = require('./getCancelPromises');
+// const {LogicSigAccount} = require('algosdk');
+
+
+jest.mock('algosdk', () => {
+  const originalModule = jest.requireActual('algosdk');
+  return {
+    ...originalModule,
+    LogicSigAccount: jest.fn((data) => 'some data').mockName('LogicSigAccount'),
+  }
+});
+
+
+const orders = {
+  rows: [
+    {
+      doc: {
+        order: {
+          escrowAddr: 'algorand_address_here',
+          contract: {data: {data: 'byte data here'}},
+        }
+      }
+    }
+]};
+
+const api = {
+  algod: 'algodObjHere',
+  wallet: 'my_algorand_wallet_here',
+  closeOrder: jest.fn(order => 'good result')
+};
+
+test('Can get cancel promises', async () => {
+  const input = {
+    escrows: orders,
+    cancelSet: new Set(['algorand_address_here']),
+    api,
+    latestPrice: 155
+  }
+  const promises = await getCancelPromises(input);
+  const goodResultObj = {
+    "escrowAddr":"algorand_address_here",
+    "contract":{
+       "data":{
+          "data":"byte data here"
+       },
+       "lsig":{
+          
+       }
+    },
+    "client":"algodObjHere",
+    "wallet":"my_algorand_wallet_here"};
+
+  expect(promises).toEqual(['good result']);
+  expect(api.closeOrder.mock.calls[0][0]).toEqual(goodResultObj);
+});
