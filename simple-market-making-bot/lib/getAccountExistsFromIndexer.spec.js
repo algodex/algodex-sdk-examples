9be11467501b3ef5a8d65e9b787d5cd37d5057e1
Alexander Trefonas <atrefonas@gmail.com>
2022-07-15 19:42:15 -0400

    Add case for account that doesn't exist

--- simple-market-making-bot/lib/getAccountExistsFromIndexer.spec.js
+++ simple-market-making-bot/lib/getAccountExistsFromIndexer.spec.js
@@ -4,15 +4,31 @@ const accountInfo = {account: {amount: 4000}};
 
 const mockIndexer = {
   lookupAccountByID: jest.fn(account => {
-    return {'do': () => new Promise(resolve => resolve(accountInfo))}
+    return {'do': () => new Promise( (resolve, reject) => {
+      if (account === 'OB2NWBH76FWNQ4IXSMESOK3MIELNJR7U7IUZNUOHKRYSDSMFI4WUDDAQ6Q') {
+        resolve(accountInfo);
+      } else {
+        reject('account does not exist');
+      }
+    })
+  }
   })
 };
 
 
-test('gets account', async () => {
+test('gets account that exists', async () => {
   //const accountInfo = await indexer.lookupAccountByID('asdsadas').do();
   const accountInfoFromIndexer =
-    await getAccountExistsFromIndexer('algorandaddress', mockIndexer);
+    await getAccountExistsFromIndexer('OB2NWBH76FWNQ4IXSMESOK3MIELNJR7U7IUZNUOHKRYSDSMFI4WUDDAQ6Q',
+    mockIndexer);
   expect(accountInfoFromIndexer).toBe(true);
-  expect(mockIndexer.lookupAccountByID.mock.calls[0][0]).toBe('algorandaddress');
+  expect(mockIndexer.lookupAccountByID.mock.calls[0][0]).toBe('OB2NWBH76FWNQ4IXSMESOK3MIELNJR7U7IUZNUOHKRYSDSMFI4WUDDAQ6Q');
+});
+
+test('gets account that doesnt exist', async () => {
+  //const accountInfo = await indexer.lookupAccountByID('asdsadas').do();
+  const accountInfoFromIndexer = await getAccountExistsFromIndexer('wrongaccount', mockIndexer);
+  expect(accountInfoFromIndexer).toBe(undefined);
+  const lastCall = mockIndexer.lookupAccountByID.mock.calls.length - 1;
+  expect(mockIndexer.lookupAccountByID.mock.calls[lastCall][0]).toBe('wrongaccount');
 });
