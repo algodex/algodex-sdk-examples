09d60871e559ab351a24bc2e4f50a03d2dd3b05c
Alexander Trefonas <atrefonas@gmail.com>
2022-07-16 17:53:23 -0400

    fix tests for runLoop

--- simple-market-making-bot/lib/runLoop.spec.js
+++ simple-market-making-bot/lib/runLoop.spec.js
@@ -1,14 +1,5 @@
 
-globalThis.currentState = {
-  latestPrice: 5, 
-  currentEscrows: ['escrowobj1', 'escrowobj2'],
-  decimals: 6 
-}
 
-globalThis.plannedOrderChanges = {
-  createEscrowPrices: [4,4.5,5,5.5],
-  cancelSet: new Set(['escrowAddr1'])
-}
 
 jest.mock('./sleep', () => jest.fn(() => {
   return new Promise((resolve) => resolve('did sleep'));
@@ -34,39 +25,91 @@ const placeOrdersAndUpdateDB = require('./placeOrdersAndUpdateDB');
 const getCurrentState = require('./getCurrentState');
 const getPlannedOrderChanges = require('./getPlannedOrderChanges');
 const cancelOrdersAndUpdateDB = require('./cancelOrdersAndUpdateDB');
+globalThis.currentState = {
+  latestPrice: 5, 
+  currentEscrows: ['escrowobj1', 'escrowobj2'],
+  decimals: 6 
+};
+globalThis.plannedOrderChanges = {
+  createEscrowPrices: [4,4.5,5,5.5],
+  cancelSet: new Set(['escrowAddr1'])
+};
 
-test('did run loop', async () => {
-  const assetInfo = null;
-  const config = 'configObj';
-  const lastBlock = null;
+describe('runLoop tests', () => {
+  afterEach(() => {
+    globalThis.currentState = {};
+    globalThis.plannedOrderChanges = {};
+    jest.clearAllMocks();
+  });
+  test('did run loop', async () => {
+    const assetInfo = null;
+    const config = 'configObj';
+    const lastBlock = null;
 
-  // Spy on run loop and stop it after first iteration
-  const validator = {
-    set: (target, key, value) => {
-      if (target.inRunLoop === true &&
-        key === 'inRunLoop' && value === false) {
-          target.isExiting = true;
-      }
-      target[key] = value;
-      return true;
-    },
-  }
-  const runState = new Proxy({inRunLoop: false, isExiting: false}
-    , validator);
-  await runLoop({assetInfo, config, lastBlock, runState});
-  const sleep_mock = sleep.mock;
-  const placeOrdersAndUpdateDB_mock = placeOrdersAndUpdateDB.mock;
-  const getCurrentState_mock = getCurrentState.mock;
-  const getPlannedOrderChanges_mock = getPlannedOrderChanges.mock;
-  const cancelOrdersAndUpdateDB_mock = cancelOrdersAndUpdateDB.mock;
+    // Spy on run loop and stop it after first iteration
+    const validator = {
+      set: (target, key, value) => {
+        if (target.inRunLoop === true &&
+          key === 'inRunLoop' && value === false) {
+            target.isExiting = true;
+        }
+        target[key] = value;
+        return true;
+      },
+    }
+    const runState = new Proxy({inRunLoop: false, isExiting: false}
+      , validator);
+    await runLoop({assetInfo, config, lastBlock, runState});
+    const sleep_mock = sleep.mock;
+    const placeOrdersAndUpdateDB_mock = placeOrdersAndUpdateDB.mock;
+    const getCurrentState_mock = getCurrentState.mock;
+    const getPlannedOrderChanges_mock = getPlannedOrderChanges.mock;
+    const cancelOrdersAndUpdateDB_mock = cancelOrdersAndUpdateDB.mock;
 
-  expect(sleep_mock.calls).toEqual([[1000]]);
-  expect(placeOrdersAndUpdateDB_mock.calls)
-    .toEqual([[{"config":"configObj","decimals":6,"latestPrice":5}]]);
-  expect(getCurrentState_mock.calls).toEqual([["configObj",null]]);
-  expect(getPlannedOrderChanges_mock.calls)
-    .toEqual([[{"config":"configObj","currentEscrows":["escrowobj1","escrowobj2"],"latestPrice":5}]]);
-  expect(cancelOrdersAndUpdateDB_mock.calls)
-    .toEqual([[{"config":"configObj","latestPrice":5,"currentEscrows":["escrowobj1","escrowobj2"]}]]);
-  return;
+    expect(sleep_mock.calls).toEqual([[1000]]);
+    expect(placeOrdersAndUpdateDB_mock.calls)
+      .toEqual([[{"config":"configObj","decimals":6,"latestPrice":5}]]);
+    expect(getCurrentState_mock.calls).toEqual([["configObj",null]]);
+    expect(getPlannedOrderChanges_mock.calls)
+      .toEqual([[{"config":"configObj","currentEscrows":["escrowobj1","escrowobj2"],"latestPrice":5}]]);
+    expect(cancelOrdersAndUpdateDB_mock.calls)
+      .toEqual([[{"config":"configObj","latestPrice":5,"currentEscrows":["escrowobj1","escrowobj2"]}]]);
+    return;
+  });
+
+  test('exits early when cant get state', async () => {
+    const assetInfo = null;
+    const config = 'configObj';
+    const lastBlock = null;
+
+    // Spy on run loop and stop it after first iteration
+    const validator = {
+      set: (target, key, value) => {
+        if (target.inRunLoop === true &&
+          key === 'inRunLoop' && value === false) {
+            target.isExiting = true;
+        }
+        target[key] = value;
+        return true;
+      },
+    }
+    const runState = new Proxy({inRunLoop: false, isExiting: false}
+      , validator);
+    await runLoop({assetInfo, config, lastBlock, runState});
+    const sleep_mock = sleep.mock;
+    const placeOrdersAndUpdateDB_mock = placeOrdersAndUpdateDB.mock;
+    const getCurrentState_mock = getCurrentState.mock;
+    const getPlannedOrderChanges_mock = getPlannedOrderChanges.mock;
+    const cancelOrdersAndUpdateDB_mock = cancelOrdersAndUpdateDB.mock;
+
+    expect(sleep_mock.calls).toEqual([[1000]]);
+    expect(placeOrdersAndUpdateDB_mock.calls)
+      .toEqual([]);
+    expect(getCurrentState_mock.calls).toEqual([["configObj",null]]);
+    expect(getPlannedOrderChanges_mock.calls)
+      .toEqual([]);
+    expect(cancelOrdersAndUpdateDB_mock.calls)
+      .toEqual([]);
+    return;
+  });
 });
