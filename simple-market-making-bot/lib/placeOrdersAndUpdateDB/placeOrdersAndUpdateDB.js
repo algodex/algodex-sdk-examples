8f37ecf263a00248859e2389e4e416b79eaeede5
Alexander Trefonas <atrefonas@gmail.com>
2022-07-16 11:32:08 -0400

    Move functions to their own files

--- simple-market-making-bot/lib/placeOrdersAndUpdateDB/placeOrdersAndUpdateDB.js
+++ simple-market-making-bot/lib/placeOrdersAndUpdateDB/placeOrdersAndUpdateDB.js
@@ -1,45 +1,13 @@
-const convertToDBObject = require('../convertToDBObject');
-const orderDepthAmounts = require('../../order-depth-amounts');
+const waitForOrders = require('./waitForOrders');
+const placeOrders = require('./placeOrders');
+const addOrdersToDB = require('./addOrdersToDB');
 
 const placeOrdersAndUpdateDB = async ({config, createEscrowPrices,
   decimals, latestPrice}) => {
-const {assetId, escrowDB, api, orderAlgoDepth} = config;
+  const ordersToPlace = placeOrders({config, createEscrowPrices, decimals, latestPrice});
+  const {validResults} = await waitForOrders(ordersToPlace);
 
-const ordersToPlace = createEscrowPrices.map(priceObj => {
-  const orderDepth = orderDepthAmounts.hasOwnProperty(''+assetId) ? 
-    orderDepthAmounts[''+assetId] : orderAlgoDepth;
-  const orderToPlace = {
-    'asset': {
-      'id': assetId, // Asset Index
-      'decimals': decimals, // Asset Decimals
-    },
-    'address': api.wallet.address,
-    'price': priceObj.price, // Price in ALGOs
-    'amount': orderDepth / latestPrice, // Amount to Buy or Sell
-    'execution': 'maker', // Type of exeuction
-    'type': priceObj.type, // Order Type
-  };
-  console.log('PLACING ORDER: ', JSON.stringify(orderToPlace), ` Latest Price: ${latestPrice}`);
-  const orderPromise = api.placeOrder(orderToPlace);
-  return orderPromise;
-});
-const results = await Promise.all(ordersToPlace.map(p => p.catch(e => e)));
-const validResults = results.filter(result => !(result instanceof Error));
-const invalidResults = results.filter(result => (result instanceof Error));
-if (invalidResults && invalidResults.length > 0) {
-  console.error({invalidResults});
-}
-const ordersAddToDB = validResults
-  .filter(order => order[0].contract.amount > 0)
-  .map(order => {
-    return escrowDB.put({
-      '_id': order[0].contract.escrow,
-      'order': convertToDBObject(order[0]),
-    });
-  });
-await Promise.all(ordersAddToDB).catch(e => {
-  console.error(e);
-});
+  await addOrdersToDB(config.escrowDB, validResults);
 }
 
 module.exports = placeOrdersAndUpdateDB;
---
+++ simple-market-making-bot/lib/placeOrdersAndUpdateDB/waitForOrders.js
@@ -0,0 +1,12 @@
+
+const waitForOrders = async (ordersToPlace) => {
+  const results = await Promise.all(ordersToPlace.map(p => p.catch(e => e)));
+  const validResults = results.filter(result => !(result instanceof Error));
+  const invalidResults = results.filter(result => (result instanceof Error));
+  if (invalidResults && invalidResults.length > 0) {
+    console.error({invalidResults});
+  }
+  return {validResults, invalidResults};
+}
+
+module.exports = waitForOrders;
