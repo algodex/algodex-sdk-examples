cdcd7b501fee9053767ddc82474297d19dd60b42
Alexander Trefonas <atrefonas@gmail.com>
2022-09-23 13:55:30 -0400

    add types

--- simple-market-making-bot/lib/cancelOrders.ts
+++ simple-market-making-bot/lib/cancelOrders.ts
@@ -1,8 +1,16 @@
-export const convertCancelResultsToDBPromises = (escrowDB, results, orders) => {
+import { AllDocsResult } from "../types/order";
+
+interface CancelResult {
+    escrowAddr:string;
+}
+
+
+export const convertCancelResultsToDBPromises = (escrowDB:any, results:CancelResult[][],
+    orders:AllDocsResult) => {
   const addrs = results.map(result => result[0].escrowAddr);
   const resultAddrs = new Set(addrs);
   const removeFromDBPromises = orders.rows
-      .filter(order => resultAddrs.has(order.doc.order.escrowAddr))
+      .filter(order => resultAddrs.has(order.doc.order.escrowAddr!))
       .map(order => escrowDB.remove(order.doc));
   if (results.length > 0) {
     console.log({results});
@@ -10,7 +18,7 @@ export const convertCancelResultsToDBPromises = (escrowDB, results, orders) => {
   return removeFromDBPromises;
 };
 
-export const cancelOrders = async (escrowDB, orders, cancelPromises) => {
+export const cancelOrders = async (escrowDB:any, orders:AllDocsResult, cancelPromises:any) => {
   return await Promise.all(cancelPromises).then(async function(results) {
     const removeFromDBPromises =
       convertCancelResultsToDBPromises(escrowDB, results, orders);
--- simple-market-making-bot/lib/placeOrdersAndUpdateDB/placeOrdersAndUpdateDB.ts
+++ simple-market-making-bot/lib/placeOrdersAndUpdateDB/placeOrdersAndUpdateDB.ts
@@ -1,7 +1,15 @@
 import waitForOrders from './waitForOrders';
 import placeOrders from './placeOrders';
 import addOrdersToDB from './addOrdersToDB';
+import { BotConfig } from '../../types/config';
+import { EscrowToMake } from '../getEscrowsToCancelAndMake';
 
+export interface PlaceOrdersAndUpdateDBInput {
+  config : BotConfig
+  createEscrowPrices: EscrowToMake[]
+  decimals: number
+  latestPrice: number
+}
 const placeOrdersAndUpdateDB = async ({config, createEscrowPrices,
   decimals, latestPrice}) => {
   const ordersToPlace =
